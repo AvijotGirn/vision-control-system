f78da09724719e94da87876360387184ab6cbd81
AvijotGirn <avijotg16@gmail.com>
2025-03-26 23:42:56 -0600

    Simple setup for UART communication to move servo left or right -> NOT YET WORKING

--- camera/test_camera.py
+++ camera/test_camera.py
@@ -1,5 +1,10 @@
 import cv2
 import numpy as np
+import serial
+
+# UART setup
+uart = serial.Serial("/dev/serial0", baudrate=115200, timeout=1)
+last_direction = None  #reduce number of calls to uart if last direction is the same as current
 
 # Open default camera (index 0)
 cap = cv2.VideoCapture(0)
@@ -48,31 +53,45 @@ while True:
             cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255),2)
             cx, cy = (x + w) // 2, (y + h) // 2 # calculating center position for the boxed object
 
+            # For moving servo to center camera across left and right movements 
+            direction = "center"
+            if cx < 100:
+                direction = "left"
+
+            elif cx > 220:
+                direction = "right"
+
+            if direction != last_direction:
+                uart.write((direction + "\n").encode())
+                print("Servo Moving: ", direction)
+                last_direction = direction
+
+            # For Positional tracking (printing movement to screen, fine tuning, etc...)
             # Euclidean distance
-            if prev_cx is not None and prev_cy is not None:
-                dx = cx - prev_cx
-                dy = cy - prev_cy 
-                distance = np.sqrt(dx**2 + dy**2)
+#            if prev_cx is not None and prev_cy is not None:
+#                dx = cx - prev_cx
+#                dy = cy - prev_cy 
+#                distance = np.sqrt(dx**2 + dy**2)
 
-                if distance > MOVEMENT_THRESHOLD:
-                    # For now we just consider L,R,U,D movement
-                    if abs(dx) > abs(dy):
-                        if dx > 0:
-                            print("Moving right.")
-                        else:
-                            print("Moving left.")
-
-                    else: # Mostly vertical movement
-                        if dy > 0:
-                            print("Moving down.")
-                        else:
-                            print("Moving up.")
-            FRAME_COUNT += 1
-            if FRAME_COUNT >= 120:
-                FRAME_COUNT = 0
-
-            if FRAME_COUNT % 2 == 0:
-                prev_cx, prev_cy = cx, cy
+#                 if distance > MOVEMENT_THRESHOLD:
+#                     # For now we just consider L,R,U,D movement
+#                     if abs(dx) > abs(dy):
+#                         if dx > 0:
+#                             print("Moving right.")
+#                         else:
+#                             print("Moving left.")
+# 
+#                     else: # Mostly vertical movement
+#                         if dy > 0:
+#                             print("Moving down.")
+#                         else:
+#                             print("Moving up.")
+#             FRAME_COUNT += 1
+#             if FRAME_COUNT >= 120:
+#                 FRAME_COUNT = 0
+# 
+#             if FRAME_COUNT % 2 == 0:
+#                 prev_cx, prev_cy = cx, cy
 
 
     cv2.imshow("Camera Feed", frame)
