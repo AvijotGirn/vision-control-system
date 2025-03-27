83d398a3de81eb3c661996c3426796df616c329b
AvijotGirn <avijotg16@gmail.com>
2025-03-26 22:03:02 -0600

    Motion tracking added (to be used with servo control)

--- camera/test_camera.py
+++ camera/test_camera.py
@@ -1,11 +1,8 @@
 import cv2
 import numpy as np
-import serial
-import time
 
 # Open default camera (index 0)
 cap = cv2.VideoCapture(0)
-uart = serial.Serial("/dev/serial0", baudrate=115200, timeout=1)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
@@ -16,60 +13,70 @@ if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
-
  #define color ranges
 lower_blue = np.array([100, 150, 100])
 upper_blue = np.array([120,255,255])
 
-lower_red1 = np.array([0,90,90])
-upper_red1 = np.array([10,255,255])
-
-lower_red2 = np.array([170,90,90])
-upper_red2 = np.array([180,255,255])
-
 BLUE_THRESHOLD = 3000
-RED_THRESHOLD = 3000
 
-last_sent = ""
+MIN_WIDTH = 25
+MIN_HEIGHT = 50
+
+FRAME_COUNT = 0
+
+# Previous object positions
+prev_cx, prev_cy = None, None
+MOVEMENT_THRESHOLD = 10 #Number of pixels to change before it counts as "movement" 
 
 while True:
-    # return value, frame -> read() returns single frame from the video 
-    # if ret false, frame could not be captured, i.e cam unplugged
     ret, frame = cap.read()
     if not ret:
         print("Failed to grab frame")
         break
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-    mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
-    mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    mask_red = cv2.bitwise_or(mask_red1, mask_red2)
 
-    # check if colors are detected 
-    blue_detected = (np.sum(mask_blue > 0) > BLUE_THRESHOLD)
-    red_detected = (np.sum(mask_red > 0) > RED_THRESHOLD)
+    contours, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    if blue_detected and red_detected:
-        message = "both"
-    elif blue_detected:
-        message = "blue"
-    elif red_detected:
-        message = "red"
-    else: 
-        message = "none"
+    if contours: 
+        largest_contour = max(contours, key=cv2.contourArea)
 
-    if message != last_sent:
-        print(f"Sending: {message}")
-        uart.write((message + "\n").encode())
-        last_sent = message
+        x, y, w, h = cv2.boundingRect(largest_contour)
+    
+        if w >= MIN_WIDTH and h > MIN_HEIGHT:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255),2)
+            cx, cy = (x + w) // 2, (y + h) // 2 # calculating center position for the boxed object
+
+            # Euclidean distance
+            if prev_cx is not None and prev_cy is not None:
+                dx = cx - prev_cx
+                dy = cy - prev_cy 
+                distance = np.sqrt(dx**2 + dy**2)
+
+                if distance > MOVEMENT_THRESHOLD:
+                    # For now we just consider L,R,U,D movement
+                    if abs(dx) > abs(dy):
+                        if dx > 0:
+                            print("Moving right.")
+                        else:
+                            print("Moving left.")
+
+                    else: # Mostly vertical movement
+                        if dy > 0:
+                            print("Moving down.")
+                        else:
+                            print("Moving up.")
+            FRAME_COUNT += 1
+            if FRAME_COUNT >= 120:
+                FRAME_COUNT = 0
+
+            if FRAME_COUNT % 2 == 0:
+                prev_cx, prev_cy = cx, cy
 
 
     cv2.imshow("Camera Feed", frame)
     cv2.imshow("Blue Mask", mask_blue)
-    cv2.imshow("Red Mask", mask_red)
 
     # Wait 1ms for key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
